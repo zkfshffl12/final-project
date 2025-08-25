e7fc2411f8da19832f0813b7152e608ccd1dba4a
zkfshffl12 <92373161+zkfshffl12@users.noreply.github.com>
2025-08-25 16:11:11 +0900

    CloudWatch 모듈 EC2 출력값 제거

--- down/terraform/modules/cloudwatch/outputs.tf
+++ down/terraform/modules/cloudwatch/outputs.tf
@@ -3,10 +3,7 @@ output "application_log_group_name" {
   value       = aws_cloudwatch_log_group.application.name
 }
 
-output "ec2_log_group_name" {
-  description = "The name of the EC2 log group"
-  value       = aws_cloudwatch_log_group.ec2.name
-}
+
 
 output "dashboard_name" {
   description = "The name of the CloudWatch dashboard"
